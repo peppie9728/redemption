b748f6be8c2c39b8eedbbd52b3a635b793d98e3f
peppie9728 <56298154+peppie9728@users.noreply.github.com>
2024-09-25 11:42:46 +0200

    bless up

--- RedemptionValley/Assets/Enemies/scripts/Beserker.cs
+++ RedemptionValley/Assets/Enemies/scripts/Beserker.cs
@@ -7,12 +7,85 @@ public class Beserker : Enemy
     public float chargeCdValue;
     float chargeCd;
 
+    public float chargeUpTimeValue;
+    public float chargeSpeed;
+
+    float chargeUpTime;
+    bool hasCharged;
+    bool isCharging;
+    Vector2 TempTarget;
+    Vector2 startPos;
+    private void Awake()
+    {
+        base.Awake();
+        chargeCd = chargeCdValue;
+        chargeUpTime = chargeUpTimeValue;
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
     public override void Attack()
     {
-        throw new System.NotImplementedException();
+        target.GetComponent<PlayerController>().AddHealth(-damage);
     }
     public override void Move()
     {
-        throw new System.NotImplementedException();
+        if (chargeCd > 0)
+        {
+            Vector2 temp = target.transform.position - transform.position;
+            temp = temp.normalized;
+            rb.velocity = new Vector2(temp.x * moveSpeed * Time.fixedDeltaTime, temp.y * moveSpeed * Time.fixedDeltaTime);
+            chargeCd -= Time.deltaTime;
+        }
+        else
+        {
+            if (!hasCharged)
+            {
+                Charge();
+            }
+            else
+            {
+                chargeCd = chargeCdValue;
+                hasCharged = false;
+            }
+        }
+    }
+
+
+    public void Charge()
+    {
+        if (chargeUpTime <= 0)
+        {
+            Vector2 temp = transform.position;
+            if (Vector2.Distance(temp,startPos) < 10)
+            {
+                rb.velocity = new Vector2(TempTarget.x * chargeSpeed * Time.fixedDeltaTime, TempTarget.y * chargeSpeed * Time.fixedDeltaTime);
+            }
+            else
+            {
+                hasCharged = true;
+                chargeUpTime = chargeUpTimeValue;
+            }
+        }
+        else
+        {
+            rb.velocity = new Vector2(0,0);
+            chargeUpTime -= Time.deltaTime;
+            TempTarget = target.transform.position - transform.position;
+            TempTarget.Normalize();
+            Vector2 temp = transform.position;
+            startPos = transform.position;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Attack();
+        }
     }
 }
